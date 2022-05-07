77de8aa0e92044ed60382890ca9eacd8c734b77c
sajal1068 <sajalkumarrewa@gmail.com>
2022-05-07 15:03:48 +0530

    nav changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -254,22 +254,115 @@ class ShapeOverlays {
 
   elmHamburger.addEventListener('click', () => {
     if (overlay.isAnimating) {
-      return false;
+      return true;
     }
     overlay.toggle();
+
     if (overlay.isOpened === true) {
+     
       elmHamburger.classList.add('is-opened-navi');
       for (var i = 0; i < gNavItems.length; i++) {
         gNavItems[i].classList.add('is-opened');
       }
+        
+      $(".global-menu__wrap").on("click", "a", function (event) {             
+          elmHamburger.classList.remove('is-opened-navi');           
+         //elmOverlay.classList.remove('is-opened');        
+          for (var i = 0; i < gNavItems.length; i++) {
+            gNavItems[i].classList.remove('is-opened');
+          }
+          overlay.close();
+        
+      });
+
     } else {
+
       elmHamburger.classList.remove('is-opened-navi');
       for (var i = 0; i < gNavItems.length; i++) {
         gNavItems[i].classList.remove('is-opened');
       }
+      elmOverlay.classList.remove('is-opened');
     }
+
+    
+
+   
+    
   });
+
+
+  
+
 }());
-          
+
+
+// top to back
+
+jQuery(window).scroll(function(){
+  if(jQuery(window).scrollTop()<50){
+      jQuery('#rocketmeluncur').slideUp(500);
+  }else{
+      jQuery('#rocketmeluncur').slideDown(500);
+  }
+  var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
+  var scrolltoprocketmeluncur = $('rocketmeluncur');
+var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
+var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
+var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
+var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
+if (basewrocketmeluncur < 1000) {
+var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
+leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
+scrolltoprocketmeluncur.style.left = ( basewrocketmeluncur + leftrocketmeluncur ) + 'px';
+} else {
+scrolltoprocketmeluncur.style.left = 'auto';
+scrolltoprocketmeluncur.style.right = '10px';
+}
+})
+
+jQuery('#rocketmeluncur').click(function(){
+  jQuery("html, body").animate({ scrollTop: '0px',display:'none'},{
+          duration: 600,  
+          easing: 'linear'
+      });
+  
+  var self = this;
+  this.className += ' '+"launchrocket";
+  setTimeout(function(){
+    self.className = 'showrocket';
+  },800)
+});
+
+/* ==============================================
+ Navigation Click & Active on Reaching Section
+ =============================================== */
+ 'use strict';
+ var sections = $('section')
+     , nav = $('nav')
+     , nav_height = nav.outerHeight();
+ 
+ $(window).on('scroll', function () {
+     'use strict';
+     var cur_pos = $(this).scrollTop();
+ 
+     sections.each(function() {
+         var top = $(this).offset().top - nav_height - 90,
+             bottom = top + $(this).outerHeight();
+ 
+         if (cur_pos >= top && cur_pos <= bottom) {
+             nav.find('a').removeClass('active');
+             sections.removeClass('active');
+ 
+             $(this).addClass('active');
+             nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+         }
+     });
+ });
+     
+ /* ==============================================
+ Calling Scroll Animations
+ =============================================== */
+'use strict';
+AOS.init();
           
   
